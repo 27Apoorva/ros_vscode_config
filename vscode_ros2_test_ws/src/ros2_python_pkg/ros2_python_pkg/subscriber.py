66cbcb91d92d797209e2d6c2b032658ca4d5c244
nilutpolkashyap <nkelectronicshlnp@gmail.com>
2023-06-29 12:03:17 +0530

    updates

--- vscode_ros2_test_ws/src/ros2_python_pkg/ros2_python_pkg/subscriber.py
+++ vscode_ros2_test_ws/src/ros2_python_pkg/ros2_python_pkg/subscriber.py
@@ -1,32 +1,28 @@
 #!/usr/bin/env python3
-import rclpy   ## ROS 2 client library for Python
-from rclpy.node import Node    ## importing Node class from rclpy
-from std_msgs.msg import String    # import built-in string message type from ROS data types
+import rclpy   
+from rclpy.node import Node    
+from std_msgs.msg import String    
 
-class HelloWorldSubscriber(Node):    ## creating a new class  inheriting from Node class
+class HelloWorldSubscriber(Node):   
 	def __init__(self):
-		super().__init__("hello_world_sub_node")     ## ==> node name
-        ## super().__init__() calls the Node class’s constructor and gives it your node name
+		super().__init__("hello_world_sub_node")    
 		self.sub = self.create_subscription(String, "hello_world", self.subscriber_callback, qos_profile=10)
-        # The node subscribes to messages of type std_msgs/String, over a topic
-        # takes 4 args (topic type, topic name, callback function qos_profile)
 
 	def subscriber_callback(self, msg):
 		print("Recieved: " + msg.data)
 
 
 def main(args=None):
-	rclpy.init(args=args)     # Initialize the rclpy library
-	my_sub = HelloWorldSubscriber()     ## create class object
+	rclpy.init(args=args)     
+	my_sub = HelloWorldSubscriber()    
 	print("Waiting for data to be published...")
 
 	try:
-		rclpy.spin(my_sub)      ### allowing the ros node to keep running
-	except KeyboardInterrupt:       ### stop the node using Ctrl + C
+		rclpy.spin(my_sub)     
+	except KeyboardInterrupt:   
 		print("Terminating Node...")
-		my_sub.destroy_node()       ### destroy the node object
-		rclpy.shutdown()     ### shutdown rclpy instances
-
+		my_sub.destroy_node()      
+		rclpy.shutdown()   
 
 if __name__ == '__main__':
 	main()
